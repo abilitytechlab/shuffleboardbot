2aec024a2659c98e17c62fa280bb7ff726d66bb0
Boris <boris@workfloor.com>
2024-01-23 17:14:06 +0100

    Fix stepper_dir_pin unknown

--- communicator/communicator_raw.py
+++ communicator/communicator_raw.py
@@ -1,8 +1,9 @@
 import time
 
+import pigpio
+
 from controller.sjoel_controller_base import MovementDirection
 from settings.raw_controls_settings import RawControlsSettings
-import pigpio
 
 
 class CommunicatorRaw:
@@ -14,7 +15,7 @@ class CommunicatorRaw:
         # Stepper
         self.pi.set_mode(self.settings.stepper_enable_pin, pigpio.OUTPUT)
         self.pi.set_mode(self.settings.stepper_step_pin, pigpio.OUTPUT)
-        self.pi.set_mode(self.settings.stepper_dir_pin, pigpio.OUTPUT)
+        self.pi.set_mode(self.settings.stepper_direction_pin, pigpio.OUTPUT)
         self.delay = 0.005 / 4
 
         # Servo
@@ -36,7 +37,7 @@ class CommunicatorRaw:
         """
         # write direction
         direction_value = 1 if direction == MovementDirection.LEFT else 0
-        self.pi.write(self.settings.stepper_dir_pin, direction_value)
+        self.pi.write(self.settings.stepper_direction_pin, direction_value)
 
         # write steps
         for _ in range(steps):
