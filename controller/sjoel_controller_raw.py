6318b793b2c3fba49c7594706c4b56391e060ff7
Boris <boris@workfloor.com>
2024-01-23 17:32:21 +0100

    Test raw controller 1

--- controller/sjoel_controller_raw.py
+++ controller/sjoel_controller_raw.py
@@ -1,15 +1,19 @@
 from communicator.communicator_raw import CommunicatorRaw
 from controller.sjoel_controller_base import SjoelControllerBase, MovementDirection
 from settings.device_settings import DeviceSettings
+import time
 
 
 class SjoelControllerRaw(SjoelControllerBase):
     def __init__(self, settings: DeviceSettings, communicator: CommunicatorRaw):
         super().__init__(settings)
         self.communicator = communicator
+        self.communicator.set_servo_angle(self.settings.fire_servo_range[0])
 
     def fire(self):
-        pass
+        self.communicator.set_servo_angle(self.settings.fire_servo_range[1])
+        time.sleep(self.settings.fire_delay)
+        self.communicator.set_servo_angle(self.settings.fire_servo_range[0])
 
     def move(self, direction: MovementDirection):
-        pass
+        self.communicator.move_stepper(direction, self.settings.stepper_steps)
