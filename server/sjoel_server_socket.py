691d2e9de720fdf53085bddcedc92b5637579ddc
Boris <boris@workfloor.com>
2024-01-23 17:26:28 +0100

    debug print

--- server/sjoel_server_socket.py
+++ server/sjoel_server_socket.py
@@ -46,6 +46,7 @@ class SjoelServerSocket(SjoelServerAbc):
 
         @self.socketio.on('fire')
         def fire():
+            print("Firing")
             try:
                 self.socketio.emit('fire', 'begin fire')
                 self.controller.fire()
