ff1a9e259cdc1d3fd97f47fffad4993d0ffc4749
Boris <boris@workfloor.com>
2024-06-04 19:37:39 +0200

    hotspot

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # Create directories and copy files
 mkdir /opt/sjoel
 cp -r . /opt/sjoel
@@ -20,6 +21,14 @@ python3.11 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 pip install gunicorn
+
+# Set up wifi hotspot
+nmcli con add type wifi ifname wlan0 con-name Hotspot autoconnect yes ssid Sjoelbak
+nmcli con modify Hotspot 802-11-wireless.mode ap 802-11-wireless.band bg ipv4.method shared
+nmcli con modify Hotspot wifi-sec.key-mgmt wpa-psk
+nmcli con modify Hotspot wifi-sec.psk "LekkerSjoelen123"
+nmcli con modify Hotspot connection.autoconnect true
+nmcli con up Hotspot
 
 # Install service
 cp sjoel.service /etc/systemd/system/
