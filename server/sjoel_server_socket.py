97371ea4640280d3d2a32ea912ca4b789dfb8f28
Boris <boris@workfloor.com>
2024-07-02 18:36:42 +0200

    Make video accessible from multiple tabs

--- server/sjoel_server_socket.py
+++ server/sjoel_server_socket.py
@@ -1,3 +1,5 @@
+from threading import Thread
+
 import cv2
 from flask import Flask, render_template, Response
 from flask_socketio import SocketIO
@@ -6,15 +8,23 @@ from controller.sjoel_controller_base import MovementDirection, SjoelControllerB
 from server.sjoel_server_abc import SjoelServerAbc
 
 
-def generate_frames(camera_index: int):
+def generate_frames(camera_index: int, frame_container: dict):
     cap = cv2.VideoCapture(camera_index)
     while True:
         ret, frame = cap.read()
         if ret:
+            index = frame_container.get(camera_index, {}).get('index', 0)
+            frame_container[camera_index] = {'frame': frame, 'index': index + 1}
+
+
+def serve_frames(camera_index: int, frame_container: dict):
+    while True:
+        frame = frame_container.get(camera_index, None)
+        if frame is not None:
             yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + cv2.imencode('.jpg', frame)[1].tobytes() + b'\r\n')
-        else:
-            break
+                   b'Content-Type: image/jpeg\r\n\r\n'
+                   + cv2.imencode('.jpg', frame['frame'])[1].tobytes()
+                   + b'\r\n')
 
 
 class SjoelServerSocket(SjoelServerAbc):
@@ -25,13 +35,21 @@ class SjoelServerSocket(SjoelServerAbc):
         self.app.route('/')(lambda: render_template('socket.html'))
         self.app.route('/video_feed/<camera_id>')(self.video_feed)
 
+        self.frame_container = {}
+        self.threads = {}
+
     def init(self):
         self._register_socketio_handlers()
         return self.app
 
-    @staticmethod
-    def video_feed(camera_id: str):
-        return Response(generate_frames(int(camera_id)), mimetype='multipart/x-mixed-replace; boundary=frame')
+    def video_feed(self, camera_id: str):
+        if camera_id not in self.threads:
+            self.threads[camera_id] = Thread(target=generate_frames, args=(int(camera_id), self.frame_container))
+            self.threads[camera_id].daemon = True
+            self.threads[camera_id].start()
+
+        return Response(serve_frames(int(camera_id), self.frame_container),
+                        mimetype='multipart/x-mixed-replace; boundary=frame')
 
     def _register_socketio_handlers(self):
         @self.socketio.on('left')
